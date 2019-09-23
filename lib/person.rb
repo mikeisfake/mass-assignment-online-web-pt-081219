d37b2a78c379381dc976cc57a4789c171d450924
Mike Cooper <mikescooper@icloud.com>
2019-09-23 02:58:47 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -5,7 +5,7 @@ class Person
 :wrist_size, :glove_size, :pant_length, :pant_width
 
 def initialize(attributes)
-  attributes.each {|k,v| self.send("#{k}=", "#{v}")}
+  attributes.each {|k,v| self.send("#{k}=", v)}
 end 
   
   
